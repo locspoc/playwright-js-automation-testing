767e4b0f48854b2adfee6131c3ce5e2f963869c3
Loc Tran <mrlocspoc@gmail.com>
2024-03-18 20:58:58 +1030

    12. Extracting the text from browser and inserting valid expect assertions in test complete

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -1,15 +1,17 @@
 const { test, expect } = require('@playwright/test');
 
-test('Browser Context Page Declaration Playwright test', async ({
+test.only('Browser Context Page Declaration Playwright test', async ({
 	browser,
 }) => {
 	const context = await browser.newContext();
 	const page = await context.newPage();
 	await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 	console.log(await page.title());
-	await page.locator('#username').fill('loctran');
+	await page.locator('#username').fill('rahulshetty');
 	await page.locator("[type='password']").fill('learning');
 	await page.locator('#signInBtn').click();
+	console.log(await page.locator("[style*='block']").textContent());
+	await expect(page.locator("[style*='block'")).toContainText('Incorrect');
 });
 
 test('Page Context Page Declaration Playwright test', async ({ page }) => {
