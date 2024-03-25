c96f7b5b3a9ed4859c1731dee89aa43f27268e76
Loc Tran <mrlocspoc@gmail.com>
2024-03-25 21:34:59 +1030

    19. Using async await with Assertions and understand validating the attributes complete

--- tests/UIBasicsTest.spec.js
+++ tests/UIBasicsTest.spec.js
@@ -31,15 +31,17 @@ test.only('UI Controls', async ({ page }) => {
 	const userName = page.locator('#username');
 	const passWord = page.locator("[type='password']");
 	const signIn = page.locator('#signInBtn');
+	const documentLink = page.locator("[href*='documents-request']");
 	const dropdown = page.locator('select.form-control');
 	await dropdown.selectOption('consult');
 	await page.locator('.radiotextsty').last().click();
 	await page.locator('#okayBtn').click();
-	await expect(page.locator('.radiotextsty').last()).toBeChecked();
 	console.log(await page.locator('.radiotextsty').last().isChecked());
+	await expect(page.locator('.radiotextsty').last()).toBeChecked();
 	await page.locator('#terms').click();
 	await expect(page.locator('#terms')).toBeChecked();
 	await page.locator('#terms').uncheck();
 	expect(await page.locator('#terms').isChecked()).toBeFalsy();
+	await expect(documentLink).toHaveAttribute('class', 'blinkingText');
 	// await page.pause();
 });
