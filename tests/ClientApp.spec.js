e9ee09500b3d9cd0a3c873f6a774a523f1229144
Loc Tran <mrlocspoc@gmail.com>
2024-03-28 19:25:04 +1030

    25. Write the Script to dynamically find the product to buy from list of products complete

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Browser Context-Validating Error login', async ({ page }) => {
+	const productName = 'Zara Coat 4';
+	const products = page.locator('.card-body');
 	await page.goto('https://www.rahulshettyacademy.com/client');
 	await page.locator('#userEmail').fill('anshika@gmail.com');
 	await page.locator('#userPassword').fill('Iamking@000');
@@ -8,6 +10,15 @@ test('Browser Context-Validating Error login', async ({ page }) => {
 	// await page.waitForLoadState('networkidle');
 	await page.locator('.card-body b').first().waitFor();
 	const titles = await page.locator('.card-body b').allTextContents();
-
 	console.log(titles);
+	const count = await products.count();
+	for (let i = 0; i < count; i++) {
+		if (
+			(await products.nth(i).locator('b').textContent()) === productName
+		) {
+			// add to cart
+			await products.nth(i).locator('text=" Add To Cart"').click();
+			break;
+		}
+	}
 });
