9a24663fa4f4a3b05b09a8047d76350c72269b6f
Loc Tran <mrlocspoc@gmail.com>
2024-03-28 22:52:43 +1030

    Section 6: End to End Web Automation Practice Exercise with Playwright complete

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -52,14 +52,27 @@ test('Add To Cart -> Checkout example', async ({ page }) => {
 			break;
 		}
 	}
-
 	// await page.pause();
 	await page.locator('.action__submit').click();
-	expect(page.locator('.hero-primary')).toHaveText(
+	await expect(page.locator('.hero-primary')).toHaveText(
 		' Thankyou for the order. '
 	);
 	const orderId = await page
 		.locator('.em-spacer-1 .ng-star-inserted')
 		.textContent();
 	console.log('orderId: ', orderId);
+	// await page.pause();
+	await page.locator("button[routerlink*='/dashboard/myorders']").click();
+	await page.locator('tbody').waitFor();
+	const rows = await page.locator('tbody tr');
+	for (let i = 0; i < (await rows.count()); i++) {
+		const rowOrderId = await rows.nth(i).locator('th').textContent();
+		if (orderId.includes(rowOrderId)) {
+			await rows.nth(i).locator('button').first().click();
+			break;
+		}
+	}
+	await page.locator('.col-text').textContent();
+	const orderIdDetails = await page.locator('.col-text').textContent();
+	expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
