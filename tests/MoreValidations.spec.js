044e3b591d3635d0964388436eba69e6aa7f8acf
Loc Tran <mrlocspoc@gmail.com>
2024-03-29 17:26:00 +1030

    39. How to automate Java/JavaScript Alert popups with Playwright complete

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -8,4 +8,9 @@ test('Popup validations', async ({ page }) => {
 	await expect(page.locator('#displayed-text')).toBeVisible();
 	await page.locator('#hide-textbox').click();
 	await expect(page.locator('#displayed-text')).toBeHidden();
+	// await page.pause();
+	// page.on('dialog', (dialog) => dialog.accept());
+	page.on('dialog', (dialog) => dialog.dismiss());
+	await page.locator('#confirmbtn').click();
+	await page.locator('#mousehover').hover();
 });
