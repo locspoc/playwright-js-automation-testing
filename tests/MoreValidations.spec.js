f3c0e46bd40249b1bb3322924cb204f24b38135e
Loc Tran <mrlocspoc@gmail.com>
2024-03-31 09:27:30 +1030

    57. Capture Screenshots with Playwright on page & partial Element level - Demo complete

--- tests/MoreValidations.spec.js
+++ tests/MoreValidations.spec.js
@@ -18,3 +18,14 @@ test('Popup validations', async ({ page }) => {
 	const textCheck = await framesPage.locator('.text h2').textContent();
 	console.log(textCheck.split(' ')[1]);
 });
+
+test.only('Screenshot & Visual comparison', async ({ page }) => {
+	await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+	await expect(page.locator('#displayed-text')).toBeVisible();
+	await page
+		.locator('#displayed-text')
+		.screenshot({ path: 'partialScreenshot.png' });
+	await page.locator('#hide-textbox').click();
+	await page.screenshot({ path: 'screenshot.png' });
+	await expect(page.locator('#displayed-text')).toBeHidden();
+});
