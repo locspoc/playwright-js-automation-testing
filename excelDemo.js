170db1827accfee44413c3493c2b1c7d7f4be003
Loc Tran <mrlocspoc@gmail.com>
2024-03-31 14:31:12 +1030

    61. Traversing rows and columns of excel worksheet with excelJS library complete

--- excelDemo.js
+++ excelDemo.js
@@ -1,3 +1,23 @@
 const ExcelJs = require('exceljs');
 
-new ExcelJs.Workbook();
+// const workbook = new ExcelJs.Workbook();
+// workbook.xlsx.readFile('./excelDownloadTest.xlsx').then(function () {
+// 	const worksheet = workbook.getWorksheet('Sheet1');
+// 	worksheet.eachRow((row, rowNumber) => {
+// 		row.eachCell((cell, colNumber) => {
+// 			console.log(cell.value);
+// 		});
+// 	});
+// });
+
+async function excelTest() {
+	const workbook = new ExcelJs.Workbook();
+	await workbook.xlsx.readFile('./excelDownloadTest.xlsx');
+	const worksheet = workbook.getWorksheet('Sheet1');
+	worksheet.eachRow((row, rowNumber) => {
+		row.eachCell((cell, colNumber) => {
+			console.log(cell.value);
+		});
+	});
+}
+excelTest();
