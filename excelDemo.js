e343c432c214949f38223fc242f773e1799ba83a
Loc Tran <mrlocspoc@gmail.com>
2024-03-31 16:47:29 +1030

    62. Build Util functions to read and update excel file strategically complete

--- excelDemo.js
+++ excelDemo.js
@@ -11,13 +11,22 @@ const ExcelJs = require('exceljs');
 // });
 
 async function excelTest() {
+	let output = { row: -1, column: -1 };
 	const workbook = new ExcelJs.Workbook();
 	await workbook.xlsx.readFile('./excelDownloadTest.xlsx');
 	const worksheet = workbook.getWorksheet('Sheet1');
 	worksheet.eachRow((row, rowNumber) => {
 		row.eachCell((cell, colNumber) => {
-			console.log(cell.value);
+			if (cell.value === 'Banana') {
+				// console.log('rowNumber: ', rowNumber);
+				// console.log('colNumber: ', colNumber);
+				output.column = colNumber;
+				output.row = rowNumber;
+			}
 		});
 	});
+	const cell = worksheet.getCell(output.row, output.column);
+	cell.value = 'Republic';
+	workbook.xlsx.writeFile('./excelDownloadTest.xlsx');
 }
 excelTest();
