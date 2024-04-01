17e7259bd770cdd8ffc2c2cde4b199daf06e8e53
Loc Tran <mrlocspoc@gmail.com>
2024-04-01 14:56:30 +1030

    72. Implementing Parameterization in running tests with different data sets complete

--- tests/EcommercePageObjects.spec.js
+++ tests/EcommercePageObjects.spec.js
@@ -5,35 +5,41 @@ const dataset = JSON.parse(
 );
 const { POManager } = require('../pageObjects/POManager');
 
-test('Add To Cart -> Checkout example', async ({ page }) => {
-	// Page Objects Manager
-	const poManager = new POManager(page);
+for (const data of dataset) {
+	test(`Add To Cart -> Checkout -> Place Order for: ${data.productName}`, async ({
+		page,
+	}) => {
+		// Page Objects Manager
+		const poManager = new POManager(page);
 
-	// Login
-	const products = page.locator('.card-body');
-	const loginPage = poManager.getLoginPage();
-	await loginPage.goTo();
-	await loginPage.validLogin(dataset.username, dataset.password);
+		// Login
+		const products = page.locator('.card-body');
+		const loginPage = poManager.getLoginPage();
+		await loginPage.goTo();
+		await loginPage.validLogin(data.username, data.password);
 
-	// Dashboard
-	const dashboardPage = poManager.getDashboardPage();
-	await dashboardPage.searchProductAddCart(dataset.productName);
-	await dashboardPage.navigateToCart();
+		// Dashboard
+		const dashboardPage = poManager.getDashboardPage();
+		await dashboardPage.searchProductAddCart(data.productName);
+		await dashboardPage.navigateToCart();
 
-	// Cart Page
-	const cartPage = poManager.getCartPage();
-	await cartPage.VerifyProductIsDisplayed(dataset.productName);
-	await cartPage.Checkout();
+		// Cart Page
+		const cartPage = poManager.getCartPage();
+		await cartPage.VerifyProductIsDisplayed(data.productName);
+		await cartPage.Checkout();
 
-	// Order Reviews Page
-	const ordersReviewPage = poManager.getOrdersReviewPage();
-	await ordersReviewPage.searchCountryAndSelect('ind', 'India');
-	const orderId = await ordersReviewPage.SubmitAndGetOrderId();
-	console.log('orderId: ', orderId);
+		// Order Reviews Page
+		const ordersReviewPage = poManager.getOrdersReviewPage();
+		await ordersReviewPage.searchCountryAndSelect('ind', 'India');
+		const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+		console.log('orderId: ', orderId);
 
-	// Order History Page
-	await dashboardPage.navigateToOrders();
-	const ordersHistoryPage = poManager.getOrdersHistoryPage();
-	await ordersHistoryPage.searchOrderAndSelect(orderId);
-	expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
-});
+		// Order History Page
+		await dashboardPage.navigateToOrders();
+		const ordersHistoryPage = poManager.getOrdersHistoryPage();
+		await ordersHistoryPage.searchOrderAndSelect(orderId);
+		expect(
+			orderId.includes(await ordersHistoryPage.getOrderId())
+		).toBeTruthy();
+	});
+}
