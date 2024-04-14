608e86ebfcf1ee708a95a3ce776ab5b472a38377
Loc Tran <mrlocspoc@gmail.com>
2024-04-14 11:10:32 +0930

    88. Create Step Definition files and associate with Cucumber.js config file complete

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,24 @@
+const { When, Then, Given } = require('@cucumber/cucumber');
+
+Given(
+	'A login to Ecommerce application with {username} and {password}',
+	function (username, password) {
+		// Write code here that turns the phrase above into concrete actions
+		return 'pending';
+	}
+);
+
+When('Add {string} to Cart', function (string) {
+	// Write code here that turns the phrase above into concrete actions
+	return 'pending';
+});
+
+Then('Verify {string} is displayed in the Cart', function (string) {
+	// Write code here that turns the phrase above into concrete actions
+	return 'pending';
+});
+
+When('Enter valid details and Place the Order', function (string) {
+	// Write code here that turns the phrase above into concrete actions
+	return 'pending';
+});
